e76429e1f3dd48fd0fa6b18e06a7546b45ca1291
ngoclma <ngoc2162003@gmail.com>
2023-09-10 06:13:17 +0800

    added: pydantic setting

--- backend/chatbot_response/services.py
+++ backend/chatbot_response/services.py
@@ -24,6 +24,18 @@ from botocore.exceptions import ClientError
 from dotenv import load_dotenv
 import csv
 
+from pydantic import BaseModel, validator
+
+class Model(BaseModel):
+    x: str = 1
+
+    @validator('x', always=True)
+    @classmethod
+    def validate_x(cls, v):
+        return v
+
+Model()
+
 collection = db.chatbot_input
 
 def csv_to_json():
